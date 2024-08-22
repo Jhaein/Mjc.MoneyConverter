0a9aa4f345bb7d2ea60de58b3dd356ce7fd03e09
Jhaein <enahj.carlos@gmail.com>
2024-08-22 12:18:55 +0800

    Created a new component by executing this command to the terminal--- 'ng generate component money-input'

--- mjc.moneyconverter.client/src/app/app.module.ts
+++ mjc.moneyconverter.client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MoneyInputComponent } from './money-input/money-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoneyInputComponent
   ],
   imports: [
     BrowserModule, HttpClientModule
