df869f07bdf3b019609da0bbe7786adb7a690c20
Jhaein <enahj.carlos@gmail.com>
2024-08-22 13:23:30 +0800

    'Added css styles in money-input and layout form input with submit button, added ng functions '

--- mjc.moneyconverter.client/src/app/money-input/money-input.component.ts
+++ mjc.moneyconverter.client/src/app/money-input/money-input.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-money-input',
   templateUrl: './money-input.component.html',
-  styleUrls: ['./money-input.component.css']
+  styleUrl: './money-input.component.css'
 })
-export class MoneyInputComponent {
 
+export class MoneyInputComponent implements OnInit {
+  moneyForm!: FormGroup;
+
+  constructor(private fb: FormBuilder,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.moneyForm = this.fb.group({
+      money: ['', Validators.required]
+    });
+  }
+
+  submitForm() {
+    const enteredAmountOfMoney = this.moneyForm.value.money;
+    this.router.navigate(['/money-converter', enteredAmountOfMoney]);
+
+  }
 }
