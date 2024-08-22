1679341cecc2ab7498e6c42306529ee15d7cd64f
Jhaein <enahj.carlos@gmail.com>
2024-08-22 10:38:14 +0800

    Added my name in app.component.html

--- Mjc.MoneyConverter.Server/obj/Debug/net8.0/Mjc.MoneyConverter.Server.AssemblyInfo.cs
+++ Mjc.MoneyConverter.Server/obj/Debug/net8.0/Mjc.MoneyConverter.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mjc.MoneyConverter.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73ebac2d0e6e1bc7dcfa56ea4804f9590d868736")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mjc.MoneyConverter.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mjc.MoneyConverter.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
