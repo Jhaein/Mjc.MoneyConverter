40c6573414779271cda2bfd4c7cff78fa4cf9010
Jhaein <enahj.carlos@gmail.com>
2024-08-22 13:45:14 +0800

    Set money-converter.ts (Injected Angular’s dependency  to get route parameters and the conversion service, subscribes to route parameters,  Manages asynchronous operations with RxJS observables to ensure data is handled

--- mjc.moneyconverter.client/src/app/money-converter/money-converter.component.ts
+++ mjc.moneyconverter.client/src/app/money-converter/money-converter.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, catchError, switchMap, throwError } from 'rxjs';
+import { MoneyConverterResponseMod } from '../shared/interface/money-converter-dto';
+import { MoneyconverterServiceService } from '../shared/services/moneyconverter-service.service';
+
 
 @Component({
   selector: 'app-money-converter',
   templateUrl: './money-converter.component.html',
-  styleUrls: ['./money-converter.component.css']
+  styleUrl: './money-converter.component.css'
 })
-export class MoneyConverterComponent {
+export class MoneyConverterComponent implements OnInit {
+  moneyInWords$: Observable<MoneyConverterResponseMod> | undefined;
 
+  constructor(private route: ActivatedRoute,
+    private moneyConverterService: MoneyconverterServiceService,
+  ) { }
+
+  ngOnInit() {
+    this.moneyInWords$ = this.route.params.pipe(
+      switchMap((params) => {
+        return this.moneyConverterService.convertAmountToWords(params['money']).pipe();
+      }),
+    );
+  }
 }
+
